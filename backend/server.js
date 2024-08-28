78267b3468c2e7e91fa9bfc4f09128335f4258cf
Atahualpa Duran <ajdm199231@gmail.com>
2024-08-27 22:42:59 -0400

    Added Basic User Email and Password Validation

--- backend/server.js
+++ backend/server.js
@@ -4,18 +4,25 @@ dotenv.config();
 import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 const port = process.env.PORT || 5000;
 
 connectDB(); // Connect to MongoDB
 
 const app = express();
 
+//Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 app.get( '/', (req, res) =>{
     res.send('API is running...');
 });
 
+//14:00
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 //middleware
 app.use(notFound);
