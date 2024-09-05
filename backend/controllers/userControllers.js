361422a3695adccc562e4a05e0cce6262b904b69
Atahualpa Duran <ajdm199231@gmail.com>
2024-09-04 22:51:37 -0400

    Added Auth Middleware and fixed Token bug

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -16,7 +16,8 @@ const authUser = asyncHandler(async (req, res) => {
             expiresIn: '30d'
         });
 
-        res.cookie('jwt','token',{
+        // Set JWT as HTTP-Only Cookie
+        res.cookie('jwt',token, {
             httpOnly: true,
             secure: process.env.NODE_ENV !== 'development',
             sameSite: 'strict',
