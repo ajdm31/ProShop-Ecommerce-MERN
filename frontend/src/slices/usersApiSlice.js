49f536e46c799d1be0a3d8628b09813e00f0460e
Atahualpa Duran <ajdm199231@gmail.com>
2024-09-09 21:45:48 -0400

    Added User Registration Functionality

--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -10,6 +10,15 @@ export const usersApiSlice = apiSlice.injectEndpoints({
                 body: data,
             }),            
         }),
+
+        register: builder.mutation({
+            query: (data) => ({
+                url: `${USERS_URL}`,
+                method: 'POST',
+                body: data,
+            }),
+        }),
+
         logout: builder.mutation({
             query: () => ({
                 url: `${USERS_URL}/logout`,
@@ -20,4 +29,4 @@ export const usersApiSlice = apiSlice.injectEndpoints({
     }),
 });
 
-export const {useLoginMutation, useLogoutMutation} = usersApiSlice;
+export const {useLoginMutation, useLogoutMutation, useRegisterMutation } = usersApiSlice;
